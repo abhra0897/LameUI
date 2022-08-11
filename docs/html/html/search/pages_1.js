4bd083d56394878ebbddc53800d5cab506145238
Avra Mitra <abhra0897@gmail.com>
2022-08-11 13:03:28 +0530

    doxygen docs build

--- docs/html/html/search/pages_1.js
+++ docs/html/html/search/pages_1.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['lameui_20documentaion_371',['LameUI Documentaion',['../index.html',1,'']]],
-  ['lameui_20_2d_20a_20lame_20_26_20lightweight_28_3f_29_20non_2dbuffered_20ui_20library_20for_20embedded_20systems_372',['LameUI - a lame &amp; lightweight(?) non-buffered UI library for embedded systems',['../md_README.html',1,'']]]
+  ['lameui_20_2d_20a_20lightweight_20gui_20library_20for_20embedded_20systems_372',['LameUI - a lightweight GUI library for embedded systems',['../md_README.html',1,'']]]
 ];
